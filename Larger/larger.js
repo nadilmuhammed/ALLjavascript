4a2c81fbc28502e307210a9c839ea95daf1c901d
nadil11 <nadilnidhal111@gmail.com>
2023-08-07 20:32:02 +0530

    uptodate

---
+++ Larger/larger.js
@@ -0,0 +1,13 @@
+
+let a,b,c;
+a = parseInt(prompt("Enter the first number"));
+b = parseInt(prompt("Enter the second number"));
+c = parseInt(prompt("Enter the third number"));
+
+let largest = Math.max(a,b,c)
+
+alert(`The largest of ${a},${b},${c} is `+largest)
+
+
+
+
